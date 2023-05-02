1b84bfbd33c547e961318996609e0d256312dfa7
unknown <aigexful@gmail.com>
2023-05-02 17:59:22 +0200

    da risolvere le relazioni

--- src/main/java/all/condominio/GestioneCondominioApplication.java
+++ src/main/java/all/condominio/GestioneCondominioApplication.java
@@ -2,8 +2,10 @@ package all.condominio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class GestioneCondominioApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/all/condominio/service/GestioneService.java
@@ -0,0 +1,9 @@
+package all.condominio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GestioneService {
+
+	
+}
